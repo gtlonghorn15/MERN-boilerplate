63482af9f9ba8f9e3bad0c03f65dd32247138a78
Ubuntu - Chris Dolan <chrisdolan55@gmail.com>
2017-12-27 21:58:33 +0000

    Adding config.js

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://EscapeRoomAdmin:ERA123@localhost:27017/my_db_name',
+  db_dev: 'mongodb://EscapeRoomAdmin:ERA123@localhost:27017/my_db_name',
+};
