5908f2613e6ea449571dd529e5143b54ceca4670
Chris Dolan <chrisdolan55@gmail.com>
2017-12-27 20:54:05 -0600

    Stars fix

--- client/app/index.js
+++ client/app/index.js
@@ -15,7 +15,7 @@ import Home from './components/Home/Home';
 
 import HelloWorld from './components/HelloWorld/HelloWorld';
 
-import StarRating from './components/Stars/Stars';
+import Stars from './components/Stars/Stars';
 
 import './styles/styles.scss';
 
