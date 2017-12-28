2aca190b2e5bf8f829e6f4c34855f4264bd7fdf4
Chris Dolan <chrisdolan55@gmail.com>
2017-12-27 20:50:59 -0600

    Switching to react-star-rating-component

--- client/app/components/Stars/Stars.js
+++ client/app/components/Stars/Stars.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-import StarRating from 'react-native-star-rating';
- 
-class Stars extends Component {
- 
-  constructor(props) {
-    super(props);
-    this.state = {
-      starCount: 3.5
-    };
-  }
- 
-  onStarRatingPress(rating) {
-    this.setState({
-      starCount: rating
-    });
-  }
- 
-  render() {
-    return (
-      <StarRating
-        disabled={false}
-        emptyStar={'ios-star-outline'}
-        fullStar={'ios-star'}
-        halfStar={'ios-star-half'}
-        iconSet={'Ionicons'}
-        maxStars={5}
-        rating={this.state.starCount}
-        selectedStar={(rating) => this.onStarRatingPress(rating)}
-        starColor={'red'}
-      />
-    );
-  }
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StarRatingComponent from 'react-star-rating-component';
+
+class Stars extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            rating: 1
+        };
+    }
+
+    onStarClick(nextValue, prevValue, name) {
+        this.setState({rating: nextValue});
+    }
+
+    render() {
+        const { rating } = this.state;
+        return (                
+            <div>
+                <h2>Rating from state: {rating}</h2>
+                <StarRatingComponent 
+                    name="rate1" 
+                    starCount={10}
+                    value={rating}
+                    onStarClick={this.onStarClick.bind(this)}
+                />
+            </div>
+        );
+    }
 }
- 
-export default Stars
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('app')
+);
+
+export default Stars;
