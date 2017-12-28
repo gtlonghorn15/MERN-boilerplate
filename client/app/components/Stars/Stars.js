2d8087e56c7475bde71852bcd855c0655e8760c9
Chris Dolan <chrisdolan55@gmail.com>
2017-12-28 16:00:25 -0600

    Adding 3 new ratings

--- client/app/components/Stars/Stars.js
+++ client/app/components/Stars/Stars.js
@@ -19,10 +19,37 @@ class Stars extends Component {
         const { rating } = this.state;
         return (                
             <div>
-                <h2>Rating from state: {rating}</h2>
+                <h2>Room Rating: {rating}</h2>
                 <StarRatingComponent 
-                    name="rate1" 
+                    name="roomrank" 
                     starCount={10}
+                    value={rating}
+                    onStarClick={this.onStarClick.bind(this)}
+                />
+            </div>
+			<div>
+                <h2>Customer Service Rating: {rating}</h2>
+                <StarRatingComponent 
+                    name="customerservicerank" 
+                    starCount={3}
+                    value={rating}
+                    onStarClick={this.onStarClick.bind(this)}
+                />
+            </div>
+			<div>
+                <h2>Immersion Rating: {rating}</h2>
+                <StarRatingComponent 
+                    name="immersionrank" 
+                    starCount={3}
+                    value={rating}
+                    onStarClick={this.onStarClick.bind(this)}
+                />
+            </div>
+			<div>
+                <h2>Puzzle Quality Rating: {rating}</h2>
+                <StarRatingComponent 
+                    name="puzzlequalityrank" 
+                    starCount={3}
                     value={rating}
                     onStarClick={this.onStarClick.bind(this)}
                 />
