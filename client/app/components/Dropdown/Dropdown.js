be421dc4198e3ef204aa6cd2fa0b51f7e8832f90
Chris Dolan <chrisdolan55@gmail.com>
2017-12-29 00:13:57 -0600

    Cleaning up dropdown code

--- client/app/components/Dropdown/Dropdown.js
+++ client/app/components/Dropdown/Dropdown.js
@@ -2,36 +2,28 @@ import React, { Component } from 'react';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 
-var options = [{ value: 'one', label: 'One' }, { value: 'two', label: 'Two' }];
+var options = [{ value: 'yes', label: 'Yes' }, { value: 'no', label: 'No' }];
 
 class Dropdown extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      selectedOption: 'Start'
+      selectedOption: 'yes'
     };
     
-    this.handleChange = this.handleChange.bind(this);
     this.updateValue = this.updateValue.bind(this);
-  }
-
-  //handleChange = (selectedOption) => {
-  handleChange(event) {
-    this.setState({selectedOption:event.target.value});
-    //console.log(`Selected: ${selectedOption.label}`);
-    alert('Choice: ' + this.state.value);
   }
   
   updateValue(newValue) {
-        this.setState({ selectedOption: newValue })
+     this.setState({ selectedOption: newValue })
+     alert('Choice: ' + this.state.value);
   }
   
   render() {
     return (
       <Select
-        name="form-field-name"
-        //value={this.state.selectedOption.value}
+        name="Room-Completed"
         value={this.state.selectedOption}
         onChange={this.updateValue}
         options={options}
