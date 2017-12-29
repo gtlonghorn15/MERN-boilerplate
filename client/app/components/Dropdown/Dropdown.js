a13ad2eebb13245a49de25b7b1503212dfebae44
Chris Dolan <chrisdolan55@gmail.com>
2017-12-29 00:09:36 -0600

    Updated dropdown

--- client/app/components/Dropdown/Dropdown.js
+++ client/app/components/Dropdown/Dropdown.js
@@ -1,34 +1,40 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+var options = [{ value: 'one', label: 'One' }, { value: 'two', label: 'Two' }];
 
 class Dropdown extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      selectedOption: '   '
+      selectedOption: 'Start'
     };
     
     this.handleChange = this.handleChange.bind(this);
+    this.updateValue = this.updateValue.bind(this);
   }
 
   //handleChange = (selectedOption) => {
   handleChange(event) {
-    this.setState({selectedOption: event.target.value});
+    this.setState({selectedOption:event.target.value});
     //console.log(`Selected: ${selectedOption.label}`);
     alert('Choice: ' + this.state.value);
+  }
+  
+  updateValue(newValue) {
+        this.setState({ selectedOption: newValue })
   }
   
   render() {
     return (
       <Select
         name="form-field-name"
-        value={this.state.selectedOption.value}
-        onChange={this.handleChange}
-        options={[
-          { value: 'one', label: 'One' },
-          { value: 'two', label: 'Two' },
-        ]}
+        //value={this.state.selectedOption.value}
+        value={this.state.selectedOption}
+        onChange={this.updateValue}
+        options={options}
       />
     );
   }
